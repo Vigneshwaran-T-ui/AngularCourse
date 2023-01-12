012418676e388194429abc1a3b104a2f4fdd7fc3
Vigneshwaran T <tvignesh835@gmail.com>
2023-01-12 22:24:46 +0530

    Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Course-Project-Recipe-Book-Shopping-Cart';
+  public headerData = 'Recipes';
+  public navRecipes = 'active';
+  public navShoppingList = ''
+
+  featureList(event){
+    this.headerData = event
+    if (this.headerData === 'Recipes'){
+      this.navRecipes = 'active';
+      this.navShoppingList = '';
+    }
+    if (this.headerData === 'shoppingList'){
+      this.navRecipes = '';
+      this.navShoppingList = 'active';
+
+    }
+  }
 }
